5f01751e011b2f60b455787f41ed3078433569da
Gabriel Alves <gabrielgabriel197@gmail.com>
2021-08-07 23:46:36 -0300

    Frontend now requests the backend for data, added post body parsing to the posts endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,24 @@
 const express = module.require('express');
 const app = express();
+const bodyParser = module.require('body-parser');
 
-app.use('/api/posts',(req, res, next) => {
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Methods', "GET, POST, PATCH, DELETE, OPTIONS, PUT");
+  next();
+});
+
+app.post('api/posts', (req, res, next) => {
+  const post = req.body;
+  res.status(201).json({
+    message: 'Post Added Successfully'
+  })
+});
+
+app.get('/api/posts',(req, res, next) => {
   const posts = [
     { id: 'aer234', title: 'test', content: 'testing the tests of tests'},
     { id: '213yu12', title: 'test', content: 'testing the tests of tests'},
