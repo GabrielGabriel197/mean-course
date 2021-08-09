c3577c9388ab178fdc37dbb5d5821aa9ce38d38d
Gabriel Alves <gabrielgabriel197@gmail.com>
2021-08-08 21:11:21 -0300

    Added image upload to back-end

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,9 +1,32 @@
 const express = module.require("express");
+const router = express.Router();
+const multer = require("multer");
+
 const Post = module.require('../models/post');
 
-const router = express.Router();
+const MIME_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpg'
+}
 
-router.post('', (req, res, next) => {
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error("Invalid mime type");
+    if (isValid) {
+      error = null;
+    }
+    cb(null, "backend/images");
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLocaleLowerCase().split(' ').join('-');
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + '-' + Date.now() + '.' + etx)
+  }
+});
+
+router.post('', multer(storage).single("image"), (req, res, next) => {
   const post = new Post({
     title: req.body.title,
     content: req.body.content
